695b9f38ecc11b04fb8b5f2268d87926e5c1ce63
sayakamik <138635063+sayakamik@users.noreply.github.com>
2023-11-14 15:00:32 +0000

    6th commit chapter4-3-3

--- sample.rb
+++ sample.rb
@@ -1,7 +1,25 @@
-puts 1+2
+# ①月曜〜日曜日までの配列（文字列）を持つ変数を作成し、その変数を使って逆の順番で表示してください。
+week_days = ["月曜", "火曜", "水曜", "木曜", "金曜", "土曜", "日曜"]
 
-a='Hello,world!'
-puts a
+reversed_week_days = week_days.reverse
+puts reversed_week_days
 
-b='こんにちは'
-puts b
+# ②1〜30の数字を順番に表示し、3の倍数でhoge、5の倍数でfoo、両方の倍数でhogefooを表示してください。
+(1..30).each do |num|
+  if num % 15 == 0
+    puts 'hogefoo'
+  elsif num % 3 == 0
+    puts 'hoge'
+  elsif num % 5 == 0
+    puts 'foo'
+  else
+    puts num.to_s
+  end
+end
+
+# １から10までを合計する
+sum = 0
+(1..10).each do |num|
+  sum += num
+end
+puts sum
