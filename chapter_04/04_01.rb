b72337b3a3010e7d7dde8bb53bedeac5d47d55fd
sayakamik <138635063+sayakamik@users.noreply.github.com>
2023-11-17 07:54:35 +0000

    7th commit chapter4-4

--- chapter_04/04_01.rb
+++ chapter_04/04_01.rb
@@ -9,3 +9,5 @@ numbers = [1, 2, 3, 4, 5]
 # ブロックの戻り値が新しい配列の各要素になる
 new_numbers = numbers.map { |n| n * 10 }
 new_numbers #=> [10, 20, 30, 40, 50]
+
+# MAP 配列を並べる
--- chapter_04/04_02.rb
+++ chapter_04/04_02.rb
@@ -9,3 +9,5 @@ numbers = [1, 2, 3, 4, 5, 6]
 # 3の倍数を除外する（3の倍数以外を集める）
 non_multiples_of_three = numbers.reject { |n| n % 3 == 0 }
 non_multiples_of_three #=> [1, 2, 4, 5]
+
+# SELECT 選び出す
